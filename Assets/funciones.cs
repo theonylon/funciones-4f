9a9ae36d3ab2a21d88db44e833dd09a4e366ea8e
theonlyone <102159023+theonylon@users.noreply.github.com>
2022-06-14 10:27:19 -0300

    eje 1 2 y 3 done

--- Assets/funciones.cs
+++ Assets/funciones.cs
@@ -7,12 +7,40 @@ public class funciones : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Destroy(gameObject);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
         
+        {
+            EscribirHolaMundoEnConsola();
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+
+        {
+            EscribirCadenaEnConsola("ACÁ TENES TU ARGUMENTO");
+        }
     }
+
+    public void DestuirObjeto()
+    {
+        Destroy(gameObject);
+    }
+
+    public void EscribirHolaMundoEnConsola()
+    {
+        Debug.Log("Hola Mundo");
+    }
+
+
+    void EscribirCadenaEnConsola(string cadena)
+    {
+        Debug.Log(cadena);
+    }
+
 }
